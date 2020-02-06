0a805f657cc78f561d2ead2b2f8e23aec47fad6c
Derek Peacock <derek_peacock@yahoo.com>
2020-02-06 15:09:33 +0000

    Add project files.

---
+++ CO453_ClassConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using CO453_ClassConsoleApp.Unit4;
+using System;
+
+namespace CO453_ClassConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double miles = 1;
+            double feet = 0;
+
+            Console.WriteLine("Derek's Distance Converter");
+
+            DistanceConverter converter = new DistanceConverter();
+
+            feet = converter.ToFeet(miles);
+            Console.WriteLine("The no of feet = " + feet);
+
+            feet = 1760 * 3;
+            miles = converter.ToMiles(feet);
+            Console.WriteLine("The no of miles = " + miles);
+        }
+    }
+}
---
+++ CO453_ClassConsoleApp/Unit4/DistanceConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CO453_ClassConsoleApp.Unit4
+{
+    /// <summary>
+    /// This class will convert miles to 
+    /// whole feet, or feet to miles
+    /// 
+    /// Author: Derek Peacock
+    /// </summary>
+    public class DistanceConverter
+    {
+        /// <summary>
+        /// This method will take a number of
+        /// miles and convert them to whole feet
+        /// </summary>
+        /// <returns></returns>
+        public double ToFeet(double miles)
+        {
+            return miles * 1760.00 * 3;
+        }
+        
+        /// <summary>
+        /// This method will take a number of
+        /// feet and convert them to miles
+        /// </summary>
+        /// <returns></returns>
+        public double ToMiles(double feet)
+        {
+            return feet / (1760.00 * 3);
+        }
+
+        /// <summary>
+        /// This method will prompt the user to enter
+        /// the number of feet or miles that they want to
+        /// convert
+        /// </summary>
+        public double GetDouble(string prompt)
+        {
+            Console.WriteLine("Please enter the number of " + prompt);
+
+            string input = Console.ReadLine();
+            double number = Convert.ToDouble(input);
+
+            return number;
+        }
+    }
+}
