654d5042126d372878a5c19d748ff71e8d6fae79
DerekPeacock <derek.peacock@yahoo.com>
2020-02-10 20:46:51 +0000

    Added Tournament and Chart

---
+++ CO453_ClassConsoleApp/Unit5/MP3Chart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CO453_ClassConsoleApp.Unit5
+{
+    class MP3Chart
+    {
+        public const int MAXN_SONGS = 5;
+        private string[] chart;
+        private int[] votes = new int[MAXN_SONGS];
+
+        public MP3Chart()
+        {
+            chart = new string[]
+            {
+                "Don't Start Now",
+                "Dance Monkey",
+                "Own It",
+                "Blinding Lights",
+                "The Box",
+                "This is Real",
+                "Good as Hell",
+                "South of the Border",
+                "Physical",
+                "Post Malone"
+            };
+        }
+
+        public void GetVotes()
+        {
+
+         }
+
+
+        public void ShowVotes()
+        {
+ 
+        }
+
+        public void ShowSongs()
+        {
+            SimpleIO.WriteTitle("UK Top 10 Chart Singles", "Task 5.2");
+
+            int position = 0;
+
+            foreach (string song in chart)
+            {
+                position++;
+                Console.WriteLine("Song " + position + " is " + song);
+            }
+        }
+    }
+}
---
+++ CO453_ClassConsoleApp/Unit5/Tournament.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CO453_ClassConsoleApp.Unit5
+{
+    class Tournament
+    {
+        const int MAXN_SCORES = 6;
+
+        // Another way of initialising attributes
+        private int[] scores = new int[MAXN_SCORES];
+
+        private string[] names;
+
+        /// <summary>
+        /// Class Constructor (not needed)
+        /// </summary>
+        public Tournament()
+        {
+            scores = new int[MAXN_SCORES];
+            names = new string[]
+            {
+                "Godfrey",
+                "Eric",
+                "Georgia",
+                "Dylan",
+                "Oluyemi",
+                "Shamial",
+                "Numan",
+                "Liam",
+                "Gita"
+            };
+        }
+
+        public void GetScores()
+        {
+            SimpleIO.WriteTitle("Enter Tournament Scores", "Task 5.1");
+
+            for (int i = 0; i < MAXN_SCORES; i++)
+            {
+                Console.Write("Enter score for player " + (i + 1) + " > ");
+                scores[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+
+        public void ShowScores()
+        {
+            SimpleIO.WriteTitle("Show Tournament Scores", "Task 5.1");
+
+            for (int i = 0; i < MAXN_SCORES; i++)
+            {
+                Console.WriteLine("Player " + (i + 1) + " scored " + scores[i]);
+            }
+        }
+    }
+}
